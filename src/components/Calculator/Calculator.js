0046bec28550fb40c97d495cf2868afeda7dfff6
Jamie Aquino <jamie.aquino33@gmail.com>
2019-11-06 13:41:03 -0600

    Added baclground image. Added container for calculator. Added some CSS to the calculator container.

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+/* Main calculator component. Contains our other calculator components. */
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="Calculator"></div>
+        );
+    }
+}
+
+export default Calculator;
