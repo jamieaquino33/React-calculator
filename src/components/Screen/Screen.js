dd31f615238e5b3f593fe0259decb3f1b6ec4326
Jamie Aquino <jamie.aquino33@gmail.com>
2019-11-07 12:39:19 -0600

    Added Computation Screen and styled it

--- src/components/Screen/Screen.js
+++ src/components/Screen/Screen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Screen.css';
 import ResultsScreen from '../ResultsScreen/ResultsScreen';
+import ComputationScreen from '../ComputationScreen/ComputationScreen';
 
 /* Component that jolds the results screen and calculation screen */
 class Screen extends Component {
@@ -8,6 +9,7 @@ class Screen extends Component {
         return (
             <div className='Screen'>
                 <ResultsScreen>0</ResultsScreen>
+                <ComputationScreen>2+3</ComputationScreen>
             </div>
         );
     }
