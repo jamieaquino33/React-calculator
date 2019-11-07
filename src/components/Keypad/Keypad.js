90462f63e250217958046c3b77e25cf2da0e197a
Jamie Aquino <jamie.aquino33@gmail.com>
2019-11-07 14:50:37 -0600

    Added buttons to Keypad. Added CSS to buttons.

--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Keypad.css';
 import KeypadRow from '../KeypadRow/KeypadRow';
+import Button from '../Button/Button';
 
 class Keypad extends Component {
     render() {
@@ -8,26 +9,45 @@ class Keypad extends Component {
             <div className='Keypad'>
                 {/*Row 1 */}
                 <KeypadRow>
+                    <Button type='primary'>C</Button>
+                    <Button type='operator'>&larr;</Button>
+                    <Button>%</Button>
+                    <Button>/</Button>
 
                 </KeypadRow>
-                
+
                 {/*Row 2*/}
                 <KeypadRow>
+                    <Button>7</Button>
+                    <Button>8</Button>
+                    <Button>9</Button>
+                    <Button>*</Button>
 
                 </KeypadRow>
 
                 {/*Row 3 */}
                 <KeypadRow>
+                <Button>4</Button>
+                <Button>5</Button>
+                <Button>6</Button>
+                <Button>-</Button>
 
                 </KeypadRow>
 
                 {/*Row 4 */}
                 <KeypadRow>
+                <Button>1</Button>
+                <Button>2</Button>
+                <Button>3</Button>
+                <Button>+</Button>
 
                 </KeypadRow>
 
                 {/*Row 5 */}
                 <KeypadRow>
+                <Button>0</Button>
+                <Button>.</Button>
+                <Button type="equalButton">=</Button>
 
                 </KeypadRow>
                
